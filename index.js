00a360b82fa59351d4610f19dec6b73c615f117a
Elijah Prosperie <elijahprosperie@gmail.com>
2022-09-26 20:50:07 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+let displayEl = document.getElementById("display1")
+let displayEl2 = document.getElementById("display2")
+let total = 0
+let totalEl = 0
+
+function touchdown() {
+touchdowns = 7
+total += touchdowns
+displayEl.textContent = total
+}
+
+function fieldGoal() {
+fieldgoals = 2
+total += fieldgoals
+displayEl.textContent = total
+}
+
+function penalty() {
+penalties = 3
+total += penalties
+displayEl.textContent = total
+}
+
+function touchdown2() {
+touchdowns = 7
+totalEl += touchdowns
+displayEl2.textContent = totalEl
+}
+
+function fieldGoal2() {
+fieldgoals = 2
+totalEl += fieldgoals
+displayEl2.textContent = totalEl
+}
+
+function penalty2() {
+penalties = 3
+totalEl += penalties
+displayEl2.textContent = totalEl
+}
